867698bda07a849bd23867f3d4103a6ba773d9d7
Mohitpg <mohitp.gupta1102@gmail.com>
2023-08-26 11:23:30 +0530

    added react bootstrap

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
+import { Button } from "./Components/Button";
+import { ChatMessage } from "./Components/ChatMessage";
+import { ScrollContainer } from "./Components/ScrollContainer";
 
-function App() {
+export default function App() {
+  const [numItems, setNumItems] = useState(5);
+
   return (
-    <div className="App">
+    <div className="h-screen w-full bg-gray-900 p-5">
+      <div className="border-2 border-white h-48 w-48 rounded-lg">
+        <ScrollContainer scrollCta="New message!">
+          {Array.from(Array(numItems).keys()).map((n) => (
+            <ChatMessage message={`Message ${n + 1}`} key={`message-${n}`} />
+          ))}
+        </ScrollContainer>
+      </div>
+      <div className="mt-2">
+        <Button onClick={() => setNumItems(numItems + 1)}>Add Item</Button>
+      </div>
     </div>
   );
 }
 
-export default App;
