bcbd738b1db1e5aefde7f2163c2d4255a6ea83ae
Mohitpg <mohitp.gupta1102@gmail.com>
2024-04-09 22:03:57 +0530

    Updated model

--- front/src/comp/ChatBox.jsx
+++ front/src/comp/ChatBox.jsx
@@ -1,9 +1,15 @@
 import "./ChatBox.css"
 export const ChatBox = (props) => {
+    const arr = props.message2.split(/<br \/>/);
+    const resultArr = [];
+    arr.forEach((item, i) => {
+      resultArr.push(item);
+      resultArr.push(<br />);     
+    });
     return (
       <div className="chatmain">
         <div className="chatuser" >{props.message}</div>
-        <div className="chatmachine">{props.message2}</div>
+        <div className="chatmachine">{resultArr}</div>
       </div>
     );
   };
--- server.py
+++ server.py
@@ -1,12 +1,14 @@
-from llama_cpp import Llama
 import whisper
 from flask import Flask,request,jsonify,render_template
 from flask_cors import CORS
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 app=Flask(__name__,static_folder="front/build/static",template_folder="front/build")
 CORS(app)
-LLM = Llama(model_path="../codes/llama-2-7b-chat.ggmlv3.q2_K.bin")
 model = whisper.load_model("small")
+tokenizer = AutoTokenizer.from_pretrained("mohitpg/llama2-mohitpg", use_auth_token=True)
+model_txt = AutoModelForCausalLM.from_pretrained("mohitpg/llama2-mohitpg", use_auth_token=True, device_map = {"": 0})
+pipe = pipeline(task="text-generation", model=model_txt, tokenizer=tokenizer, max_length=500)
 
 @app.route('/')
 def serve():
@@ -15,9 +17,11 @@ def serve():
 @app.route('/text',methods=['GET','POST'])
 def servetext():
     data = request.get_json()
-    prompt="Q: "+data[0]+" A:"
-    output=LLM(prompt,stop=["Q:"," 1."," B."])
-    return jsonify(output["choices"][0]["text"])
+    result = pipe(f"<s>[INST] {data[0]} [/INST]")
+    output=result[0]['generated_text'].split('[/INST]  ')[1]
+    l=output.split('\n')
+    output='<br />'.join(l)
+    return jsonify(output)
 
 @app.route('/sound',methods=['GET','POST'])
 def servesound():
