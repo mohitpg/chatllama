c336a745adb6f6fbb2064b959b5cca9131e80804
Mohitpg <mohitp.gupta1102@gmail.com>
2024-04-14 23:42:01 +0530

    Quantized

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 import whisper
 from flask import Flask,request,jsonify,render_template
 from flask_cors import CORS
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, BitsAndBytesConfig
 
 app=Flask(__name__,static_folder="front/build/static",template_folder="front/build")
 CORS(app)
 model = whisper.load_model("small")
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype="float16",
+    bnb_4bit_use_double_quant=False,
+)
 tokenizer = AutoTokenizer.from_pretrained("mohitpg/llama2-mohitpg", use_auth_token=True)
-model_txt = AutoModelForCausalLM.from_pretrained("mohitpg/llama2-mohitpg", use_auth_token=True, device_map = {"": 0})
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+model_txt = AutoModelForCausalLM.from_pretrained("mohitpg/llama2-mohitpg",quantization_config=bnb_config, use_auth_token=True, device_map = {"": 0})
 pipe = pipeline(task="text-generation", model=model_txt, tokenizer=tokenizer, max_length=500)
 
 @app.route('/')
