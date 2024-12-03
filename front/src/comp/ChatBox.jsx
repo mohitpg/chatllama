0516c9e4aea5dbc14c77441968ed7dc6e4aa1f45
Mohitpg <mohitp.gupta1102@gmail.com>
2024-12-03 10:43:15 +0530

    handle images

--- front/src/comp/ChatBox.jsx
+++ front/src/comp/ChatBox.jsx
@@ -1,11 +1,18 @@
 import "./ChatBox.css"
 export const ChatBox = (props) => {
-    const arr = props.message2.split(/<br \/>/);
     const resultArr = [];
-    arr.forEach((item, i) => {
-      resultArr.push(item);
-      resultArr.push(<br />);     
-    });
+    if (props.message2.slice(0,4) === "\/9j\/"){
+      resultArr.push(<img src={`data:image/jpeg;base64,${props.message2}`} width="256" height="256" style={{"display":"block" ,"marginLeft":"auto" ,"marginRight":"auto"}}/>);
+    }
+    else{
+      const arr = props.message2.split(/<br \/>/);
+      arr.forEach((item, i) => {
+        resultArr.push(item);
+        resultArr.push(<br />);     
+      });
+    }
+    
+    //console.log(resultArr)
     return (
       <div className="chatmain">
         <div className="chatuser" >{props.message}</div>
